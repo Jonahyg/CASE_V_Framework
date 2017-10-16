494aa0d610ef394bf3cd5bf37cac1df1d76943ba
George <george1d127@hotmail.com>
2017-10-16 15:19:38 -0400

    Fixed commit issues

--- MEAN/back-end/server.js
+++ MEAN/back-end/server.js
@@ -2,59 +2,22 @@ var express = require('express');
 var app = express()
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-<<<<<<< HEAD
-var Message = require('./models/Message');
-var User = require('./models/user');
-=======
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
->>>>>>> 8b7a05e2bb966fbfc83fd089eee60d35c1428578
+
+var cors = require('./services/cors');
+var checkAuthenticated = require('./services/checkAuthenticated');
 
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req, res, next)
-{
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	next();
-})
 app.get('/api/message', message.get);
 
-<<<<<<< HEAD
-app.get('/api/message', GetMessages);
-app.post('/api/message', function(req, res)
-{
-	console.log(req.body);
-	var message = new Message(req.body);
-=======
-app.post('/api/message', message.post);
->>>>>>> 8b7a05e2bb966fbfc83fd089eee60d35c1428578
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
-
-<<<<<<< HEAD
-})
-app.post('/auth/register', function(req, res)
-{
-	console.log(req.body);
-
-	var user = new User(req.body);
-
-	user.save(function(err, result))
-	{
-		if(err)
-		{
-			res.status(500).send(
-				{
-					message: err.message
-				});
-		}
-		res.status(200);
-	}
-})
-=======
->>>>>>> 8b7a05e2bb966fbfc83fd089eee60d35c1428578
+app.post('/auth/register', auth.login);
 
 
 mongoose.connect("mongodb://localhost:27017/test", function(err, db)
