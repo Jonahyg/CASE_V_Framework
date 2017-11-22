2bf212a7729f8003450534030326280b8eea97ea
George <george1d127@hotmail.com>
2017-11-22 18:02:27 -0500

    Added python script functionality

--- CASE-V/back-end/server.js
+++ CASE-V/back-end/server.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var PythonShell = require('python-shell');
+var bodyParser = require('body-parser');
 /*
 app.post('/app/message', function(req, res){
     console.log(req.body);
     res.status(200);
 })
 */
+var options = {};
+function setOptions(arguements)
+{
+	options = {
+		scriptPath: '../scripts',
+		args: arguements
+	};
+}
+app.use(function(req, res, next)
+{
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+})
+app.use(bodyParser.json());
+app.post('/api/test', function(req, res)
+{
+	setOptions(req.body.test);
+	var shell = new PythonShell("script.py", options);
+	shell.on('message', function (message)
+	{
+		res.send(message);
+	})
+	shell.end();
+})
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("we are connected to mongo");
@@ -18,3 +45,4 @@ mongoose.connect("mongodb://localhost:27017/test", function(err,db){
 var server = app.listen(5000, function(){
     console.log('listening on port ', server.address().port);
 })
+
--- CASE-V/front-end/src/app/labs/labs.controller.js
+++ CASE-V/front-end/src/app/labs/labs.controller.js
@@ -1,3 +1,17 @@
 export class LabsController {
-    
+    constructor ($scope, $http) {
+    'ngInject';
+    this.$http = $http;
+    this.$scope = $scope;
+    this.$scope.RandomString = "Hello GG";
+	}
+	click()
+	{
+		var rr = this;
+		this.$http.post('http://localhost:5000/api/test', {test: "test2"}).then(function(result)
+		{
+			console.log(result);
+			rr.$scope.RandomString = result.data;
+		})
+	}
 }
