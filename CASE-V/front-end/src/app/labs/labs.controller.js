4f6fc55def6071f9823b53861c0c785278676b7d
George <george1d127@hotmail.com>
2017-11-30 17:37:09 -0500

    front-end

--- CASE-V/front-end/src/app/labs/labs.controller.js
+++ CASE-V/front-end/src/app/labs/labs.controller.js
@@ -3,15 +3,72 @@ export class LabsController {
     'ngInject';
     this.$http = $http;
     this.$scope = $scope;
-    this.$scope.RandomString = "Hello GG";
+    this.get_images()
+    this.get_instances()
+    this.$scope.data = {
+    	model:null,
+    	stri: "",
+    	images: [],
+    	instances: []
+    };
 	}
-	click()
+	get_images()
 	{
 		var rr = this;
-		this.$http.post('http://localhost:5000/api/test', {test: "test2"}).then(function(result)
+		var items = 0;
+		this.$http.post('http://10.180.6.235:5000/api/images', {test: "test2"}).then(function(result)
 		{
-			console.log(result);
-			rr.$scope.RandomString = result.data;
+			rr.$scope.data.images = [];
+			//console.log(result);
+			items = result.data.split(" ")
+			for(var i = 0; i < items.length; i++)
+			{
+				var entry = new Object();
+				entry.id = i;
+				entry.name = items[i]
+				rr.$scope.data.images.push(entry)
+			}
+		})
+	}
+	launch_instance()
+	{
+		var rr = this;
+		if(this.$scope.data.model != null)
+		{
+			this.$http.post('http://10.180.6.235:5000/api/instance', {test: this.$scope.data.model}).then(function(result)
+			{
+				if(result.data == "ACTIVE")
+				{
+					rr.get_instances()
+				}
+			})
+		}
+	}
+	get_instances()
+	{
+		var rr = this;
+		var items = 0;
+		this.$http.post('http://10.180.6.235:5000/api/instances', {test: "test2"}).then(function(result)
+		{
+			console.log(result.data.length)
+			if(result.data.length == 0)
+			{
+				rr.$scope.data.stri = "There are no Instances to display";
+			}
+			else
+			{
+				rr.$scope.data.stri = ""
+				rr.$scope.data.instances = [];
+				//console.log(result);
+				items = result.data.split(" ")
+				for(var i = 0; i < items.length; i++)
+				{
+					var entry = new Object();
+					entry.id = i;
+					entry.name = items[i]
+					rr.$scope.data.instances.push(entry)
+				}
+			}
 		})
 	}
 }
